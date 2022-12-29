75bfaaaab96b161c45b422d35e1015940df7faf8
Eric B <compsci2fi@gmail.com>
2022-12-29 14:56:55 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+import { Player } from "./player";
+import { Game } from "./game";
+import * as Helpers from './utility';
+let newGame;
+document.getElementById('startGame').addEventListener('click', () => {
+    const player = new Player();
+    player.name = Helpers.getValue('playername');
+    const problemCount = Number(Helpers.getValue('problemCount'));
+    const factor = Number(Helpers.getValue('factor'));
+    newGame = new Game(player, problemCount, factor);
+    newGame.displayGame();
+});
+document.getElementById('calculate').addEventListener('click', () => {
+    newGame.calculateScore();
+});
+//# sourceMappingURL=app.js.map
---
+++ js/player.js
@@ -0,0 +1,6 @@
+export class Player {
+    formatName() {
+        return this.name.toUpperCase();
+    }
+}
+//# sourceMappingURL=player.js.map
